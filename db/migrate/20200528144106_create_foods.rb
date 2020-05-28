4e09d5714b1b7a4a93acd4eeba85d15a5c8862d0
Murilo Roque <muriloengqui@gmail.com>
2020-05-28 12:15:34 -0300

    edit migration tables

--- db/migrate/20200528144106_create_foods.rb
+++ db/migrate/20200528144106_create_foods.rb
@@ -1,7 +1,7 @@
 class CreateFoods < ActiveRecord::Migration[6.0]
   def change
     create_table :foods do |t|
-      t.integer :author_id
+      t.references :author, index: true, foreign_key: {to_table: :users}
       t.string :name
       t.integer :amount
 
--- db/migrate/20200528144827_add_author_to_foods.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToFoods < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :food, :author, foreign_key: { to_table: :users }
-  end
-end
