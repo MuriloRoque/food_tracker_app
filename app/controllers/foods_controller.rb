29f0135341db0738687f59a5b528a2123c357397
Murilo Roque <muriloengqui@gmail.com>
2020-05-29 08:17:44 -0300

    add working select tag when creating foods with group

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,9 +2,11 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all.order('created_at DESC')
     @total = @foods.inject(0){|sum, hash| sum + hash[:amount] }
+    # @categorized = true if Food.all.where()
   end
 
   def create
+    @groups = Group.all.order('name DESC')
     @food = current_user.foods.build(food_params)
     if @food.save
       redirect_to foods_path
@@ -21,6 +23,6 @@ class FoodsController < ApplicationController
   private
 
   def food_params
-    params.require(:food).permit(:author_id, :name, :amount)
+    params.require(:food).permit(:author_id, :name, :amount, :group_id)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   has_many :foods, foreign_key: :group_id
+
+  validates :name, presence: true, uniqueness: true, length: { in: 3..15 }
+  validates :icon, format: { with: /.+\.(gif|jpe?g|png)\z/, message: 'Please use .gif, .jpeg, .jpg or .png' }
 end
